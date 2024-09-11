67f628d543c61d8841c3753a1c48d4470ce441d6
Ion Stici <ionsticidev@gmail.com>
2024-09-11 15:09:52 +0300

    Update Readme, Add License, and Review

--- README.md
+++ README.md
@@ -1,56 +1,17 @@
-# FAQ accordion card solution
+# FAQ Accordion Card
 
 This is a solution to the [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam).
 
-## Table of contents
+This project was created as part of a learning journey in web development, so the code reflects an early stage of my experience. The FAQ Accordion Card allows users to click on questions to expand and view answers. It's a simple, interactive component **built with HTML, CSS, and JavaScript**.
 
--   [Screenshot](#screenshot)
--   [Links](#links)
--   [Built with](#built-with)
--   [Author](#author)
+## Features
 
-## Screenshot
+- **Interactive FAQ:** Clicking on a question opens the answer and collapses other open answers for a clean user experience.
+- **Responsive Layout:** The design adjust to different screen sizes for an optimal display across devices.
+
+## Screenshot and Links
+
+| [Live Demo URL](https://ionstici.github.io/faq-accordion-card/) | [Frontend Mentor](https://www.frontendmentor.io/solutions/faq-accordion-card-6Q3xSl7P07) |
+| --------------------------------------------------------------- | ---------------------------------------------------------------------------------------- |
 
 ![](./images/screenshot.png)
-
-## Links
-
--   [Solution URL](https://github.com/ionStici/faq-accordion-card)
--   [Live Site URL](https://ionstici.github.io/faq-accordion-card/)
-
-## Built with
-
--   Semantic HTML5 markup
--   Flexbox and CSS Grid
--   Some JavaScript
-
-## Author
-
--   [GitHub](https://github.com/ionStici)
--   [Frontend Mentor](https://www.frontendmentor.io/profile/ionStici)
--   [Twitter](https://twitter.com/ionStici_)
-
-<!-- #### Text
-
--   Very dark desaturated blue: hsl(238, 29%, 16%)
--   Soft red: hsl(14, 88%, 65%)
-
-#### Gradient
-
-Background gradient:
-
--   Soft violet: hsl(273, 75%, 66%)
--   Soft blue: hsl(240, 73%, 65%)
-
-#### Text
-
--   Very dark grayish blue: hsl(237, 12%, 33%)
--   Dark grayish blue: hsl(240, 6%, 50%)
-
-#### Dividers
-
--   Light grayish blue: hsl(240, 5%, 91%)
-
--   Font size: 12px
--   Family: [Kumbh Sans](https://fonts.google.com/specimen/Kumbh+Sans)
--   Weights: 400, 700 -->
--- script.js
+++ script.js
@@ -6,29 +6,29 @@ const anss = document.querySelectorAll(".card__ans");
 const icons = document.querySelectorAll(".card__icon");
 
 boxes.forEach((box) => {
-    box.addEventListener("click", function (e) {
-        const current = box.querySelector(".card__qn--active");
+  box.addEventListener("click", function (e) {
+    const current = box.querySelector(".card__qn--active");
 
-        boxes.forEach((box) => box.classList.remove("card__box--active"));
-        qns.forEach((qn) => qn.classList.remove("card__qn--active"));
-        anss.forEach((ans) => ans.classList.remove("card__ans--active"));
-        icons.forEach((icon) => icon.classList.remove("card__icon--active"));
+    boxes.forEach((box) => box.classList.remove("card__box--active"));
+    qns.forEach((qn) => qn.classList.remove("card__qn--active"));
+    anss.forEach((ans) => ans.classList.remove("card__ans--active"));
+    icons.forEach((icon) => icon.classList.remove("card__icon--active"));
 
-        const wrapper = box.closest(".card__box");
-        const qn = box.querySelector(".card__qn");
-        const ans = box.querySelector(".card__ans");
-        const icon = box.querySelector(".card__icon");
+    const wrapper = box.closest(".card__box");
+    const qn = box.querySelector(".card__qn");
+    const ans = box.querySelector(".card__ans");
+    const icon = box.querySelector(".card__icon");
 
-        wrapper.classList.add("card__box--active");
-        qn.classList.add("card__qn--active");
-        ans.classList.add("card__ans--active");
-        icon.classList.add("card__icon--active");
+    wrapper.classList.add("card__box--active");
+    qn.classList.add("card__qn--active");
+    ans.classList.add("card__ans--active");
+    icon.classList.add("card__icon--active");
 
-        if (current) {
-            wrapper.classList.remove("card__box--active");
-            qn.classList.remove("card__qn--active");
-            ans.classList.remove("card__ans--active");
-            icon.classList.remove("card__icon--active");
-        }
-    });
+    if (current) {
+      wrapper.classList.remove("card__box--active");
+      qn.classList.remove("card__qn--active");
+      ans.classList.remove("card__ans--active");
+      icon.classList.remove("card__icon--active");
+    }
+  });
 });
